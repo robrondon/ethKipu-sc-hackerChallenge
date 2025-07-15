60654a4deb0b59448198bd7008cc743f35cb4607
Robert Rondon <robrondon11@gmail.com>
2025-07-15 16:05:42 -0300

    IGrader5 interface

--- EthKipuHacker.sol
+++ EthKipuHacker.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+interface IGrader5 {
+    function retrieve() external payable;
+
+    function gradeMe(string calldata name) external;
+}
+
 contract EthKipuHacker {}
